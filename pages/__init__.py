54fea9973fc2d1c5d94a3715055632721fc394a2
SonderXiaoming <98363578+SonderXiaoming@users.noreply.github.com>
2022-05-12 12:04:56 -0500

    Add files via upload

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,4 +1,18 @@
 import nonebot
+from .pages import ma
 from .pages import hp
+from .pages import tk
+from .pages import ab
+from .pages import sc
+from .pages import js
+from .pages import st
+from .pages import qn
 app = nonebot.get_bot().server_app
+app.register_blueprint(ma)
 app.register_blueprint(hp)
+app.register_blueprint(tk)
+app.register_blueprint(ab)
+app.register_blueprint(sc)
+app.register_blueprint(js)
+app.register_blueprint(st)
+app.register_blueprint(qn)
--- pages/pages.py
+++ pages/pages.py
@@ -1,14 +1,17 @@
 import os
 import nonebot
-from quart import Blueprint,render_template
-from hoshino import Service, priv, config
+from quart import request,session,redirect,Blueprint,url_for,render_template,jsonify,session
+from nonebot.exceptions import CQHttpError
+from hoshino import R, Service, priv, config
 from pathlib import Path
 
 public_address = "127.0.0.1"#修改为服务器公网ip
 
 
 sv_help = '''
-- [帮助] 帮助页面的网页端
+- [#帮助] 帮助页面的网页端
+- [手册] 打开会战手册
+- [主页] 浏览主页
 '''.strip()
 
 sv = Service(
@@ -30,16 +33,64 @@ async def bangzhu(bot, ev):
 work_env = Path(os.path.dirname(__file__))
 homework_folder = work_env.joinpath('img')
 static_folder = work_env.joinpath('static')
+ma = Blueprint('ma',__name__,template_folder='templates',static_folder=static_folder)
 hp = Blueprint('hp',__name__,template_folder='templates',static_folder=static_folder)
+tk = Blueprint('tk',__name__,template_folder='templates',static_folder=static_folder)
+ab = Blueprint('ab',__name__,template_folder='templates',static_folder=static_folder)
+sc = Blueprint('sc',__name__,template_folder='templates',static_folder=static_folder)
+js = Blueprint('js',__name__,template_folder='templates',static_folder=static_folder)
+st = Blueprint('st',__name__,template_folder='templates',static_folder=static_folder)
+qn = Blueprint('qn',__name__,template_folder='templates',static_folder=static_folder)
 bot = nonebot.get_bot()
 app = bot.server_app
 sv.logger.info(homework_folder)
 
-@hp.route('/bot/help')
+
+
+@ma.route('/huannai/main')
+async def index():
+    return await render_template('main.html')
+
+@hp.route('/huannai/help')
 async def index():
     return await render_template('help.html')
+    
+@tk.route('/huannai/thanks')
+async def index():
+    return await render_template('thanks.html')
 
-@sv.on_fullmatch("帮助网页版",only_to_me=False)
+@ab.route('/huannai/about')
+async def index():
+    return await render_template('about.html')
+
+@sc.route('/huannai/manual')
+async def index():
+    return await render_template('manual.html')
+
+@st.route('/huannai/support')
+async def index():
+    return await render_template('support.html')
+
+@qn.route('/huannai/question')
+async def index():
+    return await render_template('question.html')
+
+
+@js.route('/huannai/404')
+async def index():
+    return await render_template('404.html')
+
+@sv.on_fullmatch("主页",only_to_me=False)
 async def get_uploader_url(bot, ev):
     cfg = config.__bot__
-    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/bot/help')
+    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/main')
+
+@sv.on_fullmatch("帮助",only_to_me=False)
+async def get_uploader_url(bot, ev):
+    cfg = config.__bot__
+    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/help')
+    
+@sv.on_fullmatch("手册",only_to_me=False)
+async def get_uploader_url(bot, ev):
+    cfg = config.__bot__
+    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/manual')
