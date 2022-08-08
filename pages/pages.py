2860523387297f6bae120e1c0526b2c929497502
kcn3388 <kcn3388@126.com>
2022-08-08 12:06:27 +0800

    add sort by bundle

--- pages/pages.py
+++ pages/pages.py
@@ -7,41 +7,42 @@ from pathlib import Path
 import markdown
 import json
 
-public_address = "127.0.0.1"#修改为服务器公网ip
-SERVICE_MODE = False # true为服务模式，即读取插件名字和插件help
-# false则读取modules文件夹下插件名字以及对应的userreadme或readme
-INVISIBLE = True # SERVICE_MODE下隐藏visible属性为false的service
-
+public_address = "127.0.0.1"  # 修改为服务器公网ip
+SERVICE_MODE = 2  # 1为服务模式，即读取插件名字和插件help
+# 2为读取bundle模式
+# 0则读取modules文件夹下插件名字以及对应的user readme或readme
+INVISIBLE = True  # SERVICE_MODE下隐藏visible属性为false的service
 
 sv_help = '''
 - [帮助] 帮助页面的网页端
 '''.strip()
 
 sv = Service(
-    name = '网页端',  #功能名
-    use_priv = priv.NORMAL, #使用权限   
-    manage_priv = priv.ADMIN, #管理权限
-    visible = True, #是否可见
-    enable_on_default = True, #是否默认启用
-    bundle = '通用', #属于哪一类
-    help_ = sv_help #帮助文本
-    )
+    name='网页端',  # 功能名
+    use_priv=priv.NORMAL,  # 使用权限
+    manage_priv=priv.ADMIN,  # 管理权限
+    visible=True,  # 是否可见
+    enable_on_default=True,  # 是否默认启用
+    bundle='通用',  # 属于哪一类
+    help_=sv_help  # 帮助文本
+)
+
 
 @sv.on_fullmatch(["帮助网页端"])
 async def bangzhu(bot, ev):
     await bot.send(ev, sv_help, at_sender=True)
-    
 
 
 work_env = Path(os.path.dirname(__file__))
 homework_folder = work_env.joinpath('img')
 static_folder = work_env.joinpath('static')
-hp = Blueprint('hp',__name__,template_folder='templates',static_folder=static_folder)
+hp = Blueprint('hp', __name__, template_folder='templates', static_folder=static_folder)
 bot = nonebot.get_bot()
 app = bot.server_app
 sv.logger.info(homework_folder)
 
 service_help = None
+
 
 @hp.route('/bot/help')
 async def index():
@@ -50,15 +51,16 @@ async def index():
         init()
     return await render_template('help.html', services=service_help)
 
-@sv.on_fullmatch("帮助网页版",only_to_me=False)
+
+@sv.on_fullmatch("帮助网页版", only_to_me=False)
 async def get_uploader_url(bot, ev):
     cfg = config.__bot__
-    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/bot/help')
+    await bot.send(ev, f'http://{public_address}:{cfg.PORT}/bot/help')
 
 
 def load_from_json(_path):
     '''读取json'''
-    data = None 
+    data = None
     if os.path.exists(_path):
         with open(_path, "r", encoding="utf-8") as f:
             data = json.load(f)
@@ -66,17 +68,20 @@ def load_from_json(_path):
         sv.logger.warning(f"{_path} 文件不存在")
     return data
 
+
 def load_black_list():
     '''获取不想被放出帮助的插件列表'''
     _path = os.path.join(os.path.dirname(__file__), 'black.json')
     black = load_from_json(_path)
     return set() if black is None else set(black)
 
+
 def load_replace_list():
     '''获取替换名字的插件列表'''
     _path = os.path.join(os.path.dirname(__file__), 'replace.json')
     replace = load_from_json(_path)
     return dict() if replace is None else replace
+
 
 def get_readme_path(module_name):
     '''获取module的帮助路径'''
@@ -91,9 +96,10 @@ def get_readme_path(module_name):
             break
     return readme_path
 
+
 def load_modules_readme():
     '''获取module的帮助'''
-    svs = [] 
+    svs = []
     black = load_black_list()
     replace = load_replace_list()
     for module in MODULES_ON:
@@ -107,6 +113,7 @@ def load_modules_readme():
         name = module if module not in replace.keys() else replace[module]
         svs.append({"name": name, "help": help})
     return svs
+
 
 def load_services_readme():
     '''获取服务的帮助'''
@@ -124,21 +131,75 @@ def load_services_readme():
         svs.append({"name": name, "help": help})
     return svs
 
+
+def load_bundle_readme():
+    bundles = Service.get_bundles()
+    services = Service.get_loaded_services()
+    data = []
+    for i in bundles:
+        helps = {
+            "bundle": i,
+            "services": []
+        }
+        for _, j in services.items():
+            if INVISIBLE and not j.visible:
+                continue
+            if j.bundle == i:
+                helping = "None" if j.help is None else j.help
+                helps["services"].append(
+                    {"name": j.name, "help": helping, "bundle": j.bundle}
+                )
+        data.append(helps)
+
+    return data
+
+
 def init():
-    '''帮助文案初始化'''
+    """帮助文案初始化"""
     global service_help
+    service_help = []
+    ids = 0
 
     if SERVICE_MODE:
         services = load_services_readme()
+        services.sort(key=lambda data: data["name"])
+        for sv2 in services:
+            # Markup防止html渲染时html的tag符号被转移
+            html = Markup(markdown.markdown(sv2["help"], extensions=[
+                'markdown.extensions.fenced_code', 'markdown.extensions.tables']))
+            service_help.append({"id": ids, "name": sv2["name"], "help": html})
+            ids += 1
+    elif SERVICE_MODE == 2:
+        bid = 0
+        bundles = load_bundle_readme()
+        for i in bundles:
+            data = {
+                "bid": bid,
+                "bundle": i["bundle"],
+                "services": []
+            }
+            for j in i["services"]:
+                html = Markup(markdown.markdown(j["help"], extensions=[
+                    'markdown.extensions.fenced_code', 'markdown.extensions.tables']))
+                data["services"].append(
+                    {
+                        "id": ids,
+                        "name": j["name"],
+                        "help": html
+                    }
+                )
+                ids += 1
+            if not len(data["services"]) == 0:
+                service_help.append(data)
+                bid += 1
     else:
         services = load_modules_readme()
-    
-    services.sort(key=lambda data:data["name"])
-    service_help = []
-    id = 0
-    for sv in services:
-        # Markup防止html渲染时html的tag符号被转移
-        html = Markup(markdown.markdown(sv["help"], extensions=[
-                      'markdown.extensions.fenced_code', 'markdown.extensions.tables']))
-        service_help.append({"id": id, "name": sv["name"], "help": html})
-        id += 1
+        services.sort(key=lambda data: data["name"])
+        for sv2 in services:
+            # Markup防止html渲染时html的tag符号被转移
+            html = Markup(markdown.markdown(sv2["help"], extensions=[
+                'markdown.extensions.fenced_code', 'markdown.extensions.tables']))
+            service_help.append({"id": ids, "name": sv2["name"], "help": html})
+            ids += 1
+
+
