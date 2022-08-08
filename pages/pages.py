97f9cb985493ea98ec32430257fe7bb0a1a68afb
SonderXiaoming <98363578+SonderXiaoming@users.noreply.github.com>
2022-08-08 16:52:57 -0500

    Add files via upload

--- pages/pages.py
+++ pages/pages.py
@@ -10,48 +10,98 @@ import markdown
 import json
 import datetime
 
-SERVICE_MODE = 0  # 0则读取modules文件夹下插件名字以及对应的user readme或readme,1为服务模式，即读取插件名字和插件help，2为读取bundle模式，
+public_address = "127.0.0.1"  # 修改为服务器公网ip
+SERVICE_MODE = 0  # 1为服务模式，即读取插件名字和插件help
+# 2为读取bundle模式
+# 0则读取modules文件夹下插件名字以及对应的user readme或readme
 INVISIBLE = True  # SERVICE_MODE下隐藏visible属性为false的service
-public_address = config.IP#修改为服务器公网ip
 
 sv_help = '''
 - [帮助] 帮助页面的网页端
-- [手册] 打开会战手册
-- [主页] 浏览主页
 - [设置最新服务]
 '''.strip()
 
 sv = Service(
-    name = '网页端',  #功能名
-    use_priv = priv.NORMAL, #使用权限   
-    manage_priv = priv.ADMIN, #管理权限
-    visible = True, #是否可见
-    enable_on_default = True, #是否默认启用
-    bundle = '通用', #属于哪一类
-    help_ = sv_help #帮助文本
-    )
+    name='网页端',  # 功能名
+    use_priv=priv.NORMAL,  # 使用权限
+    manage_priv=priv.ADMIN,  # 管理权限
+    visible=True,  # 是否可见
+    enable_on_default=True,  # 是否默认启用
+    bundle='通用',  # 属于哪一类
+    help_=sv_help  # 帮助文本
+)
+
 
 @sv.on_fullmatch(["帮助网页端"])
 async def bangzhu(bot, ev):
     await bot.send(ev, sv_help, at_sender=True)
 
+
 work_env = Path(os.path.dirname(__file__))
 homework_folder = work_env.joinpath('img')
 static_folder = work_env.joinpath('static')
-ma = Blueprint('ma',__name__,template_folder='templates',static_folder=static_folder)
-hp = Blueprint('hp',__name__,template_folder='templates',static_folder=static_folder)
-tk = Blueprint('tk',__name__,template_folder='templates',static_folder=static_folder)
-ab = Blueprint('ab',__name__,template_folder='templates',static_folder=static_folder)
-sc = Blueprint('sc',__name__,template_folder='templates',static_folder=static_folder)
-js = Blueprint('js',__name__,template_folder='templates',static_folder=static_folder)
-st = Blueprint('st',__name__,template_folder='templates',static_folder=static_folder)
-qn = Blueprint('qn',__name__,template_folder='templates',static_folder=static_folder)
+hp = Blueprint('hp', __name__, template_folder='templates', static_folder=static_folder)
 bot = nonebot.get_bot()
 app = bot.server_app
 sv.logger.info(homework_folder)
 
 service_help = None
 latest_help = None
+
+@hp.route('/bot/help')
+async def index():
+    global service_help
+    if service_help is None:
+        init()
+        if SERVICE_MODE == 2:
+            check_bundle_latest()
+        elif SERVICE_MODE == 1:
+            check_service_latest()
+        else:
+            check_module_latest()
+    return await render_template('help.html', services=service_help, SERVICE_MODE=SERVICE_MODE, latest=latest_help)
+
+
+@sv.on_fullmatch("帮助网页版", only_to_me=False)
+async def get_uploader_url(bot, ev):
+    cfg = config.__bot__
+    await bot.send(ev, f'http://{public_address}:{cfg.PORT}/bot/help')
+
+
+@sv.on_prefix("设置最新服务")
+async def set_latest(bot, ev):
+    args = ev.message.extract_plain_text()
+    services = Service.get_loaded_services()
+    if args not in services:
+        await bot.finish(ev, "不存在此服务")
+    latest_path = os.path.join(work_env, "latest.json")
+    if os.path.exists(latest_path):
+        try:
+            latest = json.load(open(latest_path, encoding="utf-8"))
+        except json.decoder.JSONDecodeError:
+            latest = []
+    else:
+        latest = []
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    exists = False
+    for each in latest:
+        if each["service"] == args:
+            each["time"] = now
+            exists = True
+    if not exists:
+        single = {
+            "service": args,
+            "time": now
+        }
+        latest.append(single)
+    with open(latest_path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(latest, indent=2, ensure_ascii=False))
+    await bot.send(ev, "设置完成")
+    if SERVICE_MODE == 2:
+        check_bundle_latest()
+    elif SERVICE_MODE == 1:
+        check_service_latest()
 
 def load_from_json(_path):
     """读取json"""
@@ -90,7 +140,6 @@ def get_readme_path(module_name):
             break
     return readme_path
 
-
 def load_modules_readme():
     """获取module的帮助"""
     svs = []
@@ -110,6 +159,7 @@ def load_modules_readme():
             svs.append({"name": name, "help": _help, "mtime": mtime, "ctime": ctime})
     return svs
 
+
 def load_services_readme():
     """获取服务的帮助"""
     svs = []
@@ -125,6 +175,7 @@ def load_services_readme():
         # name = sv.name if sv.name not in replace.keys() else replace[sv.name]
         svs.append({"name": sv1.name, "help": helping})
     return svs
+
 
 def load_bundle_readme():
     """从bundle获取帮助"""
@@ -161,6 +212,7 @@ def load_bundle_readme():
         data.append(helps)
 
     return data
+
 
 # noinspection PyTypeChecker
 # noinspection PyUnresolvedReferences
@@ -212,6 +264,7 @@ def check_service_latest():
                     data["services"].append(j)
     if not len(data["services"]) == 0:
         latest_help = data
+
 
 def init():
     """帮助文案初始化"""
@@ -268,6 +321,7 @@ def init():
             )
             ids += 1
 
+
 # noinspection PyTypeChecker
 # noinspection PyUnresolvedReferences
 def check_bundle_latest():
@@ -300,95 +354,3 @@ def check_bundle_latest():
                         data["services"].append(k)
     if not len(data["services"]) == 0:
         latest_help = data
-
-@hp.route('/huannai/help')
-async def index():
-    global service_help
-    if service_help is None:
-        init()
-        if SERVICE_MODE == 2:
-            check_bundle_latest()
-        elif SERVICE_MODE == 1:
-            check_service_latest()
-        else:
-            check_module_latest()
-    return await render_template('help.html', services=service_help, SERVICE_MODE=SERVICE_MODE, latest=latest_help)
-
-@ma.route('/huannai/main')
-async def index():
-    return await render_template('main.html')
-    
-@tk.route('/huannai/thanks')
-async def index():
-    return await render_template('thanks.html')
-
-@ab.route('/huannai/about')
-async def index():
-    return await render_template('about.html')
-
-@sc.route('/huannai/manual')
-async def index():
-    return await render_template('manual.html')
-
-@st.route('/huannai/support')
-async def index():
-    return await render_template('support.html')
-
-@qn.route('/huannai/question')
-async def index():
-    return await render_template('question.html')
-
-@js.route('/huannai/404')
-async def index():
-    return await render_template('404.html')
-
-@sv.on_fullmatch("主页",only_to_me=False)
-async def get_uploader_url(bot, ev):
-    cfg = config.__bot__
-    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/main')
-
-@sv.on_fullmatch("帮助",only_to_me=False)
-async def get_uploader_url(bot, ev):
-    cfg = config.__bot__
-    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/help')
-    
-@sv.on_fullmatch("手册",only_to_me=False)
-async def get_uploader_url(bot, ev):
-    cfg = config.__bot__
-    await bot.send(ev,f'http://{public_address}:{cfg.PORT}/huannai/manual')
-
-@sv.on_prefix("设置最新服务")
-async def set_latest(bot, ev):
-    args = ev.message.extract_plain_text()
-    services = Service.get_loaded_services()
-    if args not in services:
-        await bot.finish(ev, "不存在此服务")
-    latest_path = os.path.join(work_env, "latest.json")
-    if os.path.exists(latest_path):
-        try:
-            latest = json.load(open(latest_path, encoding="utf-8"))
-        except json.decoder.JSONDecodeError:
-            latest = []
-    else:
-        latest = []
-    now = datetime.datetime.now().strftime("%Y-%m-%d")
-
-    exists = False
-    for each in latest:
-        if each["service"] == args:
-            each["time"] = now
-            exists = True
-    if not exists:
-        single = {
-            "service": args,
-            "time": now
-        }
-        latest.append(single)
-    with open(latest_path, "w", encoding="utf-8") as f:
-        f.write(json.dumps(latest, indent=2, ensure_ascii=False))
-    await bot.send(ev, "设置完成")
-    if SERVICE_MODE == 2:
-        check_bundle_latest()
-    elif SERVICE_MODE == 1:
-        check_service_latest()
-
